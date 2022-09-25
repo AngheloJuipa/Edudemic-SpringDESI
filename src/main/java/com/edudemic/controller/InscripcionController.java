a8086130c1619eb6a73acb63f8bef029aa32568c
JoaoUF <84550001+JoaoUF@users.noreply.github.com>
2022-09-25 18:43:32 -0500

    inscripcioncontroller
    esta la es la inscripcion del controler

--- src/main/java/com/edudemic/controller/InscripcionController.java
+++ src/main/java/com/edudemic/controller/InscripcionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ch.qos.logback.core.pattern.Converter;
@@ -25,7 +26,9 @@ import com.edudemic.service.InscripcionService;
 import com.edudemic.service.MentoriaService;
 
 @Controller
-public class InscripcionController {
+public class InscripcionController 
+{
+	//editado x joao
 	private InscripcionService inscripcionService;
 	private EstudianteService estudianteService;
 	private List<Estudiante> listaEstudiantes = new ArrayList<>();
@@ -39,62 +42,68 @@ public class InscripcionController {
 		this.estudianteService=estudianteService;
 		this.mentoriaService=mentoriaService;
 	}
+	
+	
+	// WHERE: nabvarAdmin.html(58)
+	// se usa?
 	@GetMapping("/lista/inscripcion")
-	public String listarInscripciones(Model model) {
-		
+	public String listarInscripciones(Model model) 
+	{
 		model.addAttribute("inscripciones",inscripcionService.listarInscripcion());
 		return "inscripcion/listaI";
 	}
-	@GetMapping("/lista/inscripcion/estudiante/{id}")
-	public String listarNotas(@PathVariable Long id,Model model) {
 	
+	
+	// WHERE: ??
+	@GetMapping("/lista/inscripcion/estudiante/{id}")
+	public String listarNotas(@PathVariable Long id,Model model) 
+	{
 		model.addAttribute("inscripcionE",inscripcionService.listaIns(id));
 		return "estudiante/notasE";
 	}
 	
+	
+	// WHERE: listaH.html(45)
 	@GetMapping("/registro/inscripciones/{id}")
 	public String registrarInscripcionesForm(@PathVariable Long id,Model model, RedirectAttributes attribute) 
 	{
+		model.addAttribute("mentoria", mentoriaService.getMentoriaById(id));
+		model.addAttribute("inscripciones",new Inscripcion());
+		model.addAttribute("listaEstudiantes",estudianteService.listaEstudiantes());
+		model.addAttribute("listaMentorias",mentoriaService.listarMentoria());
 		
-		Inscripcion inscripcion = new Inscripcion();
-		this.listaEstudiantes = estudianteService.listaEstudiantes();
-		this.listaMentorias = mentoriaService.listarMentoria();
-		
-		
-		Mentoria st = mentoriaService.getMentoriaById(id);
-		
-	
-		
-		model.addAttribute("mentoria", st);
-		model.addAttribute("inscripciones",inscripcion);
-		model.addAttribute("listaEstudiantes",listaEstudiantes);
-		model.addAttribute("listaMentorias",listaMentorias);
 		System.out.println("Error: El id del cliente no existe");
 		attribute.addFlashAttribute("error", "Error: El id del cliente no existe");
-		
 		
 		return "inscripcion/registroI";
 	}
 	
-	@PostMapping("/inscripciones")
-	public String registrarInscripcion( @ModelAttribute("mentoria") Mentoria mentoria,@ModelAttribute("inscripcion") Inscripcion inscripcion, RedirectAttributes attribute, Model model) 
+	
+	// WHERE: registroI.html
+	@PostMapping("/inscripciones/{idEstudiante}")
+	public String registrarInscripcion(@PathVariable Long idEstudiante, @RequestParam(value="idMentoria") Long idMentoria, @ModelAttribute("mentoria") Mentoria mentoria,@ModelAttribute("inscripcion") Inscripcion inscripcion, RedirectAttributes attribute, Model model) 
 	{
+		inscripcion.setEstudiante(estudianteService.buscarPorId(idEstudiante));
+		inscripcion.setMentoria(mentoriaService.getMentoriaById(idMentoria));
 		if(inscripcionService.validarInscripción(inscripcion)==0)
-		{inscripcionService.registrarInscripcion(inscripcion);
-		  return "index";
+		{
+			inscripcionService.registrarInscripcion(inscripcion);
+			model.addAttribute("inscripcionE",inscripcionService.listaIns(idEstudiante));
+			return "inscripcion/listaIE";
 		}
 		else
 		{
-			model.addAttribute("mensaje", "El estudiante ya se inscribió en mentorias 3 veces");
+			model.addAttribute("mensaje", "En esta mentoria ya se ha inscrito 1 estudiante");
 			return "inscripcion/registroI";
 		}
-		
 	}
-	@GetMapping("/lista/inscripcion/estudiante2/{id}")
-    public String listar(@PathVariable Long id,Model model) {
 
+
+	// WHERE: navbar.html(68)
+	@GetMapping("/lista/inscripcion/estudiante2/{id}")
+    public String listar(@PathVariable Long id,Model model) 
+	{
         model.addAttribute("inscripcionE",inscripcionService.listaIns(id));
         return "inscripcion/listaIE";
     }
-
 }
