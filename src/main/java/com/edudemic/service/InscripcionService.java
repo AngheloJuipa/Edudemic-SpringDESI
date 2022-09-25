ba01dc105c7d77c38e0a92d99a390d93f5b5a773
JoaoUF <84550001+JoaoUF@users.noreply.github.com>
2022-09-25 18:48:49 -0500

    inscipción service
    esta es la inscripción service

--- src/main/java/com/edudemic/service/InscripcionService.java
+++ src/main/java/com/edudemic/service/InscripcionService.java
@@ -9,51 +9,73 @@ import com.edudemic.entities.Inscripcion;
 import com.edudemic.entities.Mentoria;
 import com.edudemic.repository.InscripcionRepository;
 @Service
-public class InscripcionService {
+public class InscripcionService 
+{
+	//editado x joao
 	private InscripcionRepository inscripcionRepository;
+	
 	public InscripcionService(InscripcionRepository inscripcionRepository) 
 	{
 		this.inscripcionRepository=inscripcionRepository;
 	}
+	
+	//LIST
 	public List<Inscripcion> listarInscripcion() 
 	{
 		return inscripcionRepository.findAll();
 	}
+	
+	//REGISTER
 	public Inscripcion registrarInscripcion(Inscripcion c) 
 	{
 		return inscripcionRepository.save(c);
 	}
+	
+	//DELETE
+	public void borrarInscripcion(Long id)
+	{
+		inscripcionRepository.deleteById(id);
+	}
+	
+	//LIST BY studiante
 	public List<Inscripcion> listaIns(Long id)
 	{
 		return inscripcionRepository.listaIns(id);
 	}
-	public Inscripcion getInscripcionById(Long id) {
+	
+	//SEARCH BY ID
+	public Inscripcion getInscripcionById(Long id) 
+	{
 		return inscripcionRepository.findById(id).get();
 	}
 	
+	//MAX 1 INSCRIPCIONES
 	public int validarInscripción(Inscripcion inscripcion)
 	{
 		int validar=0;
 		int contador=0;
 		List<Inscripcion> listaAux=new ArrayList<>();
 		listaAux=inscripcionRepository.findAll();
+		
 		if(listaAux.size()>0)
-		{for(int i=0;i<listaAux.size();i++)
 		{
-			if(inscripcion.getEstudiante().getId()==listaAux.get(i).getEstudiante().getId() && inscripcion.getMentoria().getFecha()==listaAux.get(i).getMentoria().getFecha())
+			for(int i=0;i<listaAux.size();i++)
 			{
-				contador++;
+				if(inscripcion.getMentoria().getId()==listaAux.get(i).getMentoria().getId())
+				{
+					contador++;
+				}
+				if(contador==1)
+				{
+					validar=1;
+					break;
+				}
 			}
-			
-			if(contador==3)
-			{
-				validar=1;
-				break;
-			}
-		}
 		}
 		else
-			validar=0;
+		{
+			validar=0;		
+		}
 		contador=0;
 		return validar;
 	}
