5d6e5d7294d3b3f79cc03e300ae55484d5b231be
JoaoUF <84550001+JoaoUF@users.noreply.github.com>
2022-09-25 18:47:37 -0500

    mentoria repository
    esta es la mentoria repository

--- src/main/java/com/edudemic/repository/MentoriaRepository.java
+++ src/main/java/com/edudemic/repository/MentoriaRepository.java
@@ -7,13 +7,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface MentoriaRepository extends JpaRepository<Mentoria, Long> {
+public interface MentoriaRepository extends JpaRepository<Mentoria, Long> 
+{
+	//editado x joao
 	@Query("SELECT m FROM Mentoria m WHERE m.id=?1")
 	List<Mentoria> listaMen(Long id);
 	
 	@Query("SELECT m FROM Mentoria m WHERE m.id=?1")
 	Mentoria objetoM(Long id);
+	
 	List<Mentoria> findByFechaContainingIgnoreCase(String fecha);
+	
 	@Query("SELECT m FROM Mentoria m WHERE m.profesor.id=?1")
     List<Mentoria> listaMentoriasxProfesor(Long id);
 }
